f6402132f817fa7770fd0c3ecc45f61b647e26cb
moraloyuxing <s110319023@stu.ntue.edu.tw>
2019-05-10 01:36:40 +0800

    0510_bullet

--- SlimeAndTheirHappyBros/Assets/Scripts/Nig/YEEEEE.cs
+++ SlimeAndTheirHappyBros/Assets/Scripts/Nig/YEEEEE.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class YEEEEE : MonoBehaviour{
     public Player_Control Player_1;
+
+    Dictionary<string, Bullet_Behaviour> bullets = new Dictionary<string, Bullet_Behaviour>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < transform.childCount; i++) {
+            Transform bullet = transform.GetChild(i);
+            bullets.Add(bullet.name, bullet.GetComponent<Bullet_Behaviour>());
+           
+        }
+
+    }
+
     void Start(){
         
     }
 
     void Update(){
         if (Input.GetKeyDown(KeyCode.Q)) Player_1.GetRescued();
+       
     }
+
+    public Bullet_Behaviour GetBulletByName( string name) {
+        if (bullets.ContainsKey(name))
+        {
+            return bullets[name];
+        }
+        else return null;
+    }
+
 }
