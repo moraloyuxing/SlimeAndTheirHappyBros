9af872d3c7bca2d75f1fda6f4844ee68b1742f4a
dyronzha <dyron85511@gmail.com>
2019-05-12 16:40:38 +0800

    color test

---
+++ SlimeAndTheirHappyBros/Assets/Scripts/Zha/ColorTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 6; i++) {
+            transform.GetChild(i).GetComponent<SpriteRenderer>().material.SetInt("_colorID", i+1);
+            transform.GetChild(i + 6).GetComponent<SpriteRenderer>().material.SetInt("_colorID", i+1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
