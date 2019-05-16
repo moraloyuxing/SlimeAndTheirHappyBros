1c841286178cb4a9f449d53a3d52782620fed2be
moraloyuxing <s110319023@stu.ntue.edu.tw>
2019-05-16 11:08:46 +0800

    0516_damnittt

--- SlimeAndTheirHappyBros/Assets/Scripts/Nig/Bullet_Manager.cs
+++ SlimeAndTheirHappyBros/Assets/Scripts/Nig/Bullet_Manager.cs
@@ -5,19 +5,51 @@ using UnityEngine;
 public class Bullet_Manager : MonoBehaviour{
 
     Dictionary<string, Bullet_Behaviour> bullets = new Dictionary<string, Bullet_Behaviour>();
+    private Queue<GameObject> _bulletpool = new Queue<GameObject>();
+    public GameObject prefabBullet;//怕不夠......= =
 
     void Awake(){
         for (int i = 0; i < transform.childCount; i++){
             Transform bullet = transform.GetChild(i);
+            //存入Dictonary
             bullets.Add(bullet.name, bullet.GetComponent<Bullet_Behaviour>());
+
+            //存入Queue
+            bullet.GetComponent<Bullet_Behaviour>().SetPool(this);
+            _bulletpool.Enqueue(bullet.gameObject);
         }
     }
 
+    //子彈取程式碼by Dictionary
     public Bullet_Behaviour GetBulletByName(string name){
         if (bullets.ContainsKey(name)){
             return bullets[name];
         }
         else return null;
     }
+
+    //子彈使用與回收
+    public void Bullet_ReUse(Vector3 position,Quaternion rotation,Vector3 current_angle,GameObject xSlime,int Shader_Number) {
+        if (_bulletpool.Count > 0){
+            GameObject reuse = _bulletpool.Dequeue();
+            reuse.transform.position = position;
+            reuse.transform.rotation = rotation;
+            reuse.SetActive(true);
+            reuse.GetComponent<Bullet_Behaviour>().SetAttackDir(current_angle, xSlime,Shader_Number);
+        }
+
+        else {
+            GameObject clone_bullet = Instantiate(prefabBullet) as GameObject;
+            clone_bullet.transform.position = position;
+            clone_bullet.transform.rotation = rotation;
+        }
+
+    }
+
+    public void Bullet_Recovery(GameObject recovery) {
+        _bulletpool.Enqueue(recovery);
+        recovery.SetActive(false);
+    }
+
 
 }
--- SlimeAndTheirHappyBros/Assets/Scripts/Nig/Create_Bullet.cs
+++ SlimeAndTheirHappyBros/Assets/Scripts/Nig/Create_Bullet.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Create_Bullet : MonoBehaviour{
     public GameObject Player;
     public GameObject Hint_Arrow;
-    public Object_Pool bulletPool;
+    //public Object_Pool bulletPool;
+    public Bullet_Manager _bulletPool;
     Vector3 shooting_direction;
 
-    public void SetMergeSlimePool(Object_Pool pool){
-        bulletPool = pool;
+    public void SetMergeSlimePool(Bullet_Manager pool){
+        _bulletPool = pool;
     }
 
-    void ShootBullet(Vector3 current_angle) {
-        bulletPool.ReUse(Player.transform.position, Hint_Arrow.transform.rotation, current_angle,Player);
+    public void ShootBullet(Vector3 current_angle,int Shader_Number) {
+        _bulletPool.Bullet_ReUse(Player.transform.position, Hint_Arrow.transform.rotation, current_angle,Player,Shader_Number);
     }
 
 }
