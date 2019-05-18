8c353576a98b8bb3565833736ef10b2acf0322e0
Halaa <neil841004@gmail.com>
2019-05-18 23:56:47 +0800

    Auto stash before merge of "master" and "origin/master"
    Title Scene

---
+++ SlimeAndTheirHappyBros/Assets/Scripts/Han/JoinGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinGame : MonoBehaviour
+{
+    public Camera m_camera;
+    public int pNumber;  
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.LookAt(transform.position + m_camera.transform.rotation * Vector3.forward, m_camera.transform.rotation * Vector3.up);
+        if(Input.GetKeyDown(KeyCode.A)){
+            animator.SetBool("join",true);
+        }
+    }
+}
