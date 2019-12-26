2d47ba4f13a94ea293edae21d44ee7ee3372c51c
Halaa <neil841004@gmail.com>
2019-12-26 21:41:00 +0800

    icon閃爍

---
+++ SlimeAndTheirHappyBros/Assets/Scripts/Han/FlashIcon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashIcon : MonoBehaviour
+{
+    Color color;
+    bool changeA = true;
+    // Use this for initialization
+    void Start()
+    {
+        color = GetComponent<Image>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flash();
+    }
+    void Flash()
+    {
+        if (color.a <= 0.3f)
+        {
+            changeA = false;
+        }
+        if (color.a >= 0.7)
+        {
+            changeA = true;
+        }
+        if (color.a >= 0.3f && changeA)
+        {
+            color.a -= 0.015f;
+        }
+        if (color.a <= 0.7f && !changeA)
+        {
+            color.a += 0.015f;
+        }
+        GetComponent<Image>().color = color;
+    }
+}
